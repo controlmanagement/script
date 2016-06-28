2e547040a75ece5fc469f7841b2f9ec2274dec3b
Mikito-Kohno <mikito@a.phys.nagoya-u.ac.jp>
2016-06-28 12:15:05 +0900

    CLT 6/27 デバッグ後更新
    更新箇所
    インデントの変更

--- otf_test.py
+++ otf_test.py
@@ -1,5 +1,4 @@
 #-*- coding: utf-8 -*-
-#presented by Hiroaki Iwamura
 import time
 import math
 import antenna_nanten
@@ -46,16 +45,16 @@ class otf(object):
 		f.close()
 		
 		#param[2]=on_x, param[3]=on_y, param[4]=off_x, param[5]=off_y, param[6]=coord_mode, param[12]=start_x, param[13]=start_y, param[19]=n,
-		#param[18]/(param[16]/param[15])=dt, param[25]/(param[16]/param[15])=rampt
+		#param[18]/param[16]=dt, param[25]/param[16]=rampt
 		#delay=0 => for adjust time(OFF to ON time)
 		if int(param[14]) == 0:
-			dx = float(param[18])/3600.
+			dx = float(param[18])
 			dy = 0
 		else:
 			dx = 0
-			dy = float(param[18])/3600.
-		dt = float(param[18])/(float(param[16])/float(param[15]))
-		rampt = float(param[25])/(float(param[16])/float(param[15]))
+			dy = float(param[18])
+		dt = float(param[18])/float(param[16])
+		rampt = float(param[25])/float(param[16])
 		
 		param[6] = param[6].lstrip('"')
 		param[6] = param[6].rstrip('"')
@@ -65,19 +64,26 @@ class otf(object):
 		else:
 			coord_sys = param[6].upper()
 			coord_mode = 0
-			lamda = c/(float(param[39])*math.pow(10, 6))
+			pass
+		lamda = c/(float(param[39])*math.pow(10, 6))
+        	print("coord_sys = "+coord_sys)
 		total_count = int(param[19])
 		
 		for scan_count in range(total_count):
 			#OFF data
+			print(coord_sys, param)
 			if coord_sys == "EQUATRIAL":
-				self.antenna.radec_move(float(param[4]), float(param[5]), coord_mode, lamda, float(param[20]), float(param[21]), hosei)
-			elif coord_sys == "GALACTIC":
-				self.antenna.galactic_move(float(param[4]), float(param[5]), lamda, float(param[20]), float(param[21]), hosei)
+				self.antenna.radec_move(float(param[4]), float(param[5]), coord_mode, float(param[20]), float(param[21]), hosei)
 				time.sleep(0.1)
+			elif coord_sys == "GALACTIC":
+				self.antenna.galactic_move(float(param[4]), float(param[5]), coord_mode, float(param[20]), float(param[21]), hosei)
+				time.sleep(0.1)
+            		else:
+                		print("!!coord_sys is EQUATRIAL or GALACTIC!!")
+                		return
 			ret = self.antenna.read_track()
 			print("observe off")
-			print("ret="+ret[0]+" "+ret[1])
+			print("ret="+str(ret[0])+" "+str(ret[1]))
 			while ret[0] == "FALSE" or ret[1] == "FALSE":
 				ret = self.antenna.read_track()
 				time.sleep(0.5)
@@ -85,33 +91,27 @@ class otf(object):
 			self.antenna.tracking_end()
 			
 			print("b")
+			
 			#ON data
+			if coord_sys == "EQUATRIAL":
+				self.antenna.radec_move(float(param[2]), float(param[3]), coord_mode, float(param[0]), float(param[1]), hosei)
+			elif coord_sys == "GALACTIC":
+				self.antenna.galactic_move(float(param[2]), float(param[3]), coord_mode, float(param[0]), float(param[1]), hosei)
+			time.sleep(0.1)
+			ret = self.antenna.read_track()
+			print("observe on")
+			print("ret="+str(ret[0])+" "+str(ret[1]))
+			while ret[0] == "FALSE" or ret[1] == "FALSE":
+				ret = self.antenna.read_track()
+				if self.stop_thread.is_set():
+					sys.exit()
+				time.sleep(0.5)
 			if int(param[14]) == 0:
 				sx = float(param[2]) + float(param[12])/3600.
 				sy = float(param[3]) + float(param[13])/3600. + dx/3600.*scan_count
 			else:
 				sx = float(param[2]) + float(param[12])/3600. + dy/3600.*scan_count
 				sy = float(param[3]) + float(param[13])/3600.
-			
-			start_x = sx-float(dx)/2.-float(dx)/float(dt)*rampt
-			start_y = sy-float(dy)/2.-float(dy)/float(dt)*rampt
-			
-			print("start_x:"+str(start_x))
-			print("start_y:"+str(start_y))
-			
-			if coord_sys == "EQUATRIAL":
-				self.antenna.radec_move(start_x, start_y, coord_mode, lamda, float(param[0]), float(param[1]), hosei)
-			elif coord_sys == "GALACTIC":
-				self.antenna.galactic_move(start_x, start_y, lamda, float(param[0]), float(param[1]), hosei)
-			time.sleep(0.1)
-			ret = self.antenna.read_track()
-			print("observe on")
-			print("ret="+ret[0]+" "+ret[1])
-			while ret[0] == "FALSE" or ret[1] == "FALSE":
-				ret = self.antenna.track_check()
-				if self.stop_thread.is_set():
-					sys.exit()
-				time.sleep(0.5)
 			self.antenna.otf_tracking_end()
 			stime = (40587 + time.time()/(24.*3600.))+(0+rampt)/24./3600. # 0 = delay ,for test
 			#self.get_data(stime,???)    or stime from self.antenna.otf_start
@@ -127,6 +127,7 @@ class otf(object):
 			
 		self.antenna.otf_end()
 		return
+
 
 
 
