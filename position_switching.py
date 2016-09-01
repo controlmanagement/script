d381d53351e79ec7dc5f24d60377baa9560867c0
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2016-09-01 12:49:09 +0900

    Update position_switching.py
    ドップラーのパス変更、_2NDLOの調整など

--- position_switching.py
+++ position_switching.py
@@ -39,8 +39,6 @@ if args.tau is not None: tau = args.tau
 import os
 import time
 import numpy
-import sys
-sys.path.append("/home/amigos/NECRX_system/base_param")
 import doppler_nanten
 dp = doppler_nanten.doppler_nanten()
 import controller
@@ -80,6 +78,11 @@ if obs['otadel'].lower() == 'y':
     offset_dcos = 1
 else:
     offset_dcos = 0
+#if obs['otadel_off'].lower() == 'y':
+    #offset_dcos_off = 1
+#else:
+    #offset_dcos_off = 0
+
 if obs['coordsys'].lower() == 'j2000' or 'b1950':
     coord_sys = 'EQUATRIAL'
     move = con.radec_move
@@ -151,12 +154,29 @@ while num < n:
         
     move(offx, offy, obs['coordsys'], off_x=obs['lamdel_off'], off_y=obs['betdel_off'])
     print('moving...')
+
     while not con.read_track():
         time.sleep(0.001)
         continue
+    status = con.read_status()
+    dome_az = status["Current_Dome"]
+    if dome_az < 0.:
+        dome_az += 360.
+    ant_az = status["Current_Az"]
+    if ant_az < 0.:
+        ant_az += 360.
+    while abs(dome_az - ant_az) > 3. and abs(dome_az - ant_az) < 357.:
+        time.sleep(0.5)
+        status = con.read_status()
+        dome_az = status["Current_Dome"]
+        if dome_az < 0.:
+            dome_az += 360.
+        ant_az = status["Current_Az"]
+        if ant_az < 0.:
+            ant_az += 360.
+
     print('tracking OK')
     _now = time.time()
-    dp1 = 0
     if _now > latest_hottime+60*obs['load_interval']:
         print('R')
         con.move_hot('in')
@@ -166,8 +186,10 @@ while num < n:
         print('Temp: %.2f'%(temp))
         
         print('get spectrum...')
+        #dp1 = dp.set_track(83.80613,-5.374320,0,"J2000",0,0,0,"J2000",0,230.5380,220.3986765,1,-1,8.038000000000,9.301318999999)
         dp1 = dp.set_track(obs['lambda_on'], obs['beta_on'], obs['vlsr'], obs['coordsys'], obs['lamdel'], obs['betdel'], offset_dcos, obs['coordsys'], integ_off*2+integ_on, obs['restfreq_1']/1000., obs['restfreq_2']/1000., sb1, sb2, 8038.000000000/1000., 9301.318999999/1000.)
         #lambel_off,betdel_offかも？SYNTHが固定値の場合
+        #print(dp1)
         d = con.oneshot(exposure=integ_off)
         d1 = d['dfs1'][0]
         d2 = d['dfs2'][0]
@@ -194,8 +216,8 @@ while num < n:
         latest_hottime = time.time()
         P_hot = numpy.sum(d1)
         tsys_list.append(0)
-        _2NDLO_list1.append(dp1[3]['sg21'])
-        _2NDLO_list2.append(dp1[3]['sg22'])
+        _2NDLO_list1.append(dp1[3]['sg21']*1000)
+        _2NDLO_list2.append(dp1[3]['sg22']*1000)
         pass
 
 
@@ -203,11 +225,13 @@ while num < n:
     con.move_hot('out')
     
     print('get spectrum...')
-    if dp1 == 0:
+    if latest_hottime > _now:
+        pass
+    else:
+        #dp1 = dp.set_track(83.80613,-5.374320,0,"J2000",0,0,0,"J2000",0,230.5380,220.3986765,1,-1,8.038000000000,9.301318999999)
         dp1 = dp.set_track(obs['lambda_on'], obs['beta_on'], obs['vlsr'], obs['coordsys'], obs['lamdel'], obs['betdel'], offset_dcos, obs['coordsys'], integ_off+integ_on, obs['restfreq_1']/1000., obs['restfreq_2']/1000., sb1, sb2, 8038.000000000/1000., 9301.318999999/1000.)
         #lambel_off,betdel_offかも？SYNTHが固定値の場合
-    else:
-        pass
+    #print(dp1)
     temp = float(con.read_status()['CabinTemp1']) + 273.15
     d = con.oneshot(exposure=integ_off)
     d1 = d['dfs1'][0]
@@ -235,8 +259,8 @@ while num < n:
     P_sky = numpy.sum(d1)
     tsys = temp/(P_hot/P_sky-1)
     tsys_list.append(tsys)
-    _2NDLO_list1.append(dp1[3]['sg21'])
-    _2NDLO_list2.append(dp1[3]['sg22'])
+    _2NDLO_list1.append(dp1[3]['sg21']*1000)
+    _2NDLO_list2.append(dp1[3]['sg22']*1000)
 
 
     print('move ON')
@@ -247,11 +271,29 @@ while num < n:
     while not con.read_track():
         time.sleep(0.001)
         continue
+    status = con.read_status()
+    dome_az = status["Current_Dome"]
+    if dome_az < 0.:
+        dome_az += 360.
+    ant_az = status["Current_Az"]
+    if ant_az < 0.:
+        ant_az += 360.
+    while abs(dome_az - ant_az) > 3. and abs(dome_az - ant_az) < 357.:
+        time.sleep(0.5)
+        status = con.read_status()
+        dome_az = status["Current_Dome"]
+        if dome_az < 0.:
+            dome_az += 360.
+        ant_az = status["Current_Az"]
+        if ant_az < 0.:
+            ant_az += 360.
+
     print('tracking OK')
     
     print('ON')     
 
     print('get spectrum...')
+    #print(dp1)
     temp = float(con.read_status()['CabinTemp1']) + 273.15
     d = con.oneshot(exposure=integ_on)
     d1 = d['dfs1'][0]
@@ -277,8 +319,8 @@ while num < n:
     secofday_list.append(con.read_status()['Secofday'])
     subref_list.append(con.read_status()['Current_M2'])
     tsys_list.append(tsys)
-    _2NDLO_list1.append(dp1[3]['sg21'])    
-    _2NDLO_list2.append(dp1[3]['sg22'])
+    _2NDLO_list1.append(dp1[3]['sg21']*1000)    
+    _2NDLO_list2.append(dp1[3]['sg22']*1000)
 
     print('stop')
     con.tracking_end()
@@ -510,7 +552,7 @@ read2 = {
     "SIDEBAND" : obs['lo1st_sb_2'],
     "_2NDSB" : obs['lo2nd_sb_2'],
     "_3RDSB" : obs['lo3rd_sb_2'],
-    "_2NDLO" : _2NDLO_list2,#ドップラーシフト込み                                  
+    "_2NDLO" : _2NDLO_list2,#ドップラーシフト込み              
     "_3RDLO" : obs['lo3rd_freq_2'],
     "SUBREF" : subref_list,
     "LOCKSTAT" : 'F'#未使用                                                    
@@ -526,5 +568,3 @@ numpy.save(f2+".npy",read2)
 import n2fits_write
 n2fits_write.write(read1,f1)
 n2fits_write.write(read2,f2)
-
-
